d8a78ea772120c82abfbd911fce866d728b31428
Arnoud Visser <physar@gmail.com>
2021-10-08 13:57:54 +0200

    Update logic_link.sh

--- pal_camera/etc/dreamvu/logic_link.sh
+++ pal_camera/etc/dreamvu/logic_link.sh
@@ -2,12 +2,12 @@
 
 pal_sdk_dir=$1
 
-cd $pwd/include/dreamvu
+cd ./include/dreamvu
 ln -s $pal_sdk_dir/include/PAL.h .
 ln -s $pal_sdk_dir/include/PAL_Image.h .
 ln -s $pal_sdk_dir/include/PAL_CameraProperties.h .
 
-cd $pwd/lib/dreamvu
+cd ./lib/dreamvu
 ln -s $pal_sdk_dir/lib/libPAL.so .
 ln -s $pal_sdk_dir/lib/libPAL_CAMERA.so .
 ln -s $pal_sdk_dir/lib/libPAL_DEPTH.so .
