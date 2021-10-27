89c50e098c285fc093a6e656440de173212996d6
Arnoud Visser <physar@gmail.com>
2021-10-27 14:34:17 +0200

    Looking if the while loop is run multiple times

--- pal_camera/src/pal_camera_node.cpp
+++ pal_camera/src/pal_camera_node.cpp
@@ -499,7 +499,7 @@ void PalCameraNode::grab_loop()
           mPubCloud->publish(pointcloudMsg);
       }
 
-      RCLCPP_INFO_ONCE(get_logger(), "End of first Grab loop");
+      RCLCPP_INFO(get_logger(), "End of Grab loop");
 
   } while( rclcpp::ok() ); // end of while(1)
 
