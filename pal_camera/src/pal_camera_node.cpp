45bf25c1b20484bf7b7e0a9cd6689a12f5a3f47a
Arnoud Visser <physar@gmail.com>
2021-10-27 14:51:39 +0200

    Removed again the hack to always publish the left image

--- pal_camera/src/pal_camera_node.cpp
+++ pal_camera/src/pal_camera_node.cpp
@@ -78,7 +78,7 @@ PalCameraNode::PalCameraNode(const rclcpp::NodeOptions& options)
   , mVideoQos(1)
 {
   RCLCPP_INFO(get_logger(), "********************************");
-  RCLCPP_INFO(get_logger(), "      DreamVU PAL Camera v1.1.6     ");
+  RCLCPP_INFO(get_logger(), "      DreamVU PAL Camera v1.1.7     ");
   RCLCPP_INFO(get_logger(), "********************************");
   RCLCPP_INFO(get_logger(), " * namespace: %s", get_namespace());
   RCLCPP_INFO(get_logger(), " * node name: %s", get_name());
@@ -400,7 +400,7 @@ void PalCameraNode::grab_loop()
      publishBase2PalCameraTransform(timeStamp);
 
      // ----> Publish the left image if someone has subscribed to
-      if (leftSubnumber >= 0)
+      if (leftSubnumber > 0)
       {
           RCLCPP_INFO_ONCE(get_logger(), "Publishing first left-image of the  PAL camera");
           cv::Mat mat_left = cv::Mat(g_imgLeft.rows, g_imgLeft.cols, CV_8UC3, g_imgLeft.Raw.u8_data);
